a5d3014af05aa2546f23bc35aaa1baa160c75d5b
Franklinjsb <franklinjunior412@gmail.com>
2025-07-24 02:53:29 +0000

    BatalhaNaval_Nível_Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -13,11 +13,11 @@ int main() {
     char linha_índice[5]={'A','B','C','D','E'};
     int coluna =1;
 
-    printf("%s\n",linha_índice);
+    printf("  %2s\n",linha_índice);
 
     
         for (int i = 0; i < 5; i++) {
-            printf("%d",coluna);
+            printf("%d ",coluna);
             for (int j = 0; j < 5; j++) {
                 if (i==1 && j>0 || i>1 && j==4)
                 {
@@ -32,11 +32,37 @@ int main() {
             coluna = coluna+ 1;
         }
     
-
+        printf("\n");
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+
+    int tabuleiro_aventureiro[10][10];
+    char linha_índice_2[10]={'A','B','C','D','E','F','G','H','I','J'};
+    int coluna_2 =1;
+
+    printf("   %s\n",linha_índice_2);
+
+    
+        for (int i = 0; i < 10; i++) {
+            printf("%2d ",coluna_2);
+            for (int j = 0; j < 10; j++) {
+                if (i==j&& i<5 || i==j && i>6)
+                {
+                    tabuleiro_aventureiro[i][j] = 3;
+                }
+                else{
+                tabuleiro_aventureiro[i][j] = 0;  
+                }
+                printf("%d", tabuleiro_aventureiro[i][j]);
+            }
+            printf("\n");
+            coluna_2 = coluna_2+ 1;
+        }
+    
+
+
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
